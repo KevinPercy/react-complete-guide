a43e1e50c9c9b3ac817c86c9fd9562aa53dba4e3
KevinPercy <kevinpercycl@gmail.com>
2020-04-22 20:57:26 -0500

    Se agrega el componente persona basado en funciones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {//reserved for Component based classes
+    persons: [
+      { name: 'Kevin', age: 24 },
+      { name: 'Percy', age: 23 },
+      { name: 'Kaissa', age:21 }
+    ]
+  }
+  
   render(){
-  //  return (
-  //   <div className="App">
-  //     <h1>Hi, I'm a React App</h1>
-  //   </div>
-  //  ); 
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));  
+   return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbie cycling</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    </div>
+   ); 
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));  
   }
 }
 

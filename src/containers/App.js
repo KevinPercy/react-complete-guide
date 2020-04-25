5defb253569bbfebe534a9be91b2e92a4a8e1195
KevinPercy <kevinpercycl@gmail.com>
2020-04-24 20:41:24 -0500

    Se divide la aplicacion en componentes para optimizar la app

---
+++ src/containers/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+//import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
+
+class App extends Component {
+  state = { // reserved for Component based classes
+    persons: [
+      { id: '1', name: 'Kevin', age: 24 },
+      { id: '2', name: 'Percy', age: 23 },
+      { id: '3', name: 'Kaissa', age: 21 }
+    ],
+    otherState: 'this is other value',
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow })
+  }
+
+  render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
+        </div>
+      );
+
+
+    }
+
+
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+        {persons}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));  
+  }
+}
+
+export default App;

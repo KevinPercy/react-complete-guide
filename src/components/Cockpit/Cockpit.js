11b1f62055153c78fdc95019c358687446d5f2cf
KevinPercy <kevinpercycl@gmail.com>
2020-04-25 22:28:39 -0500

    Se agregan componentes de grado mayor(HOC) para envolver los demas componentes

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Cockpit.css';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxiliary';
 
 const cockpit = (props) => {
     const assignedClasses = [];

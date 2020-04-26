2a096202de91b581c7c7b9c110a491568de33afe
KevinPercy <kevinpercycl@gmail.com>
2020-04-25 22:21:45 -0500

    Se agregan componentes de grado mayor(HOC) para envolver los demas componentes

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if (props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
     if (props.persons.length <= 2) {
         assignedClasses.push(classes.red);
@@ -14,13 +15,13 @@ const cockpit = (props) => {
         assignedClasses.push(classes.bold);
     }
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toogle Persons</button>
-        </div>
+        </Aux>
     );
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 //import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 
 class App extends PureComponent {
@@ -100,7 +101,7 @@ class App extends PureComponent {
 
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showPersons:true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -109,7 +110,7 @@ class App extends PureComponent {
           clicked={this.togglePersonsHandler}
         />
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));  
   }
